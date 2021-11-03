c92d404247308c3b68af58a1307419d5986a4a0c
Ramaniuk <240724oi@gmail.com>
2021-11-03 15:59:07 -0400

    first commit

---
+++ module_4/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en",
+                     help="Choose language: for example: fr, ru etc.")
+
+@pytest.fixture()
+def language(request):    
+    language = request.config.getoption("language")
+    if language == None:
+        raise pytest.UsageError("--language should be passed")
+    return language
+
+
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
